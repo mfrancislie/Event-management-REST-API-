2369913f6e2781853f225d8c7b45cbd87f4ecb5b
Francislie <mfrancislie@gmail.com>
2024-03-08 10:53:46 -0800

    delete and update data

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -30,7 +30,7 @@ class EventController extends Controller
             ]),
             'user_id' => 1
         ]);
-        
+
         return $event;
     }
 
@@ -45,16 +45,26 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+         $event->update(
+          $request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
+            ])
+        );
+        
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+        return response(status: 204);
     }
 }
