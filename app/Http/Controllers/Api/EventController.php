d922157af8e21eb219ab3bcfdad2b34740ee91fb
Francislie <mfrancislie@gmail.com>
2024-03-11 09:50:22 -0700

    api resources(controlling json response)

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,8 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with('user')->get());
+        // return EventResource::collection(Event::all()); without user
     }
 
     /**
@@ -31,7 +33,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -39,7 +41,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -56,7 +59,7 @@ class EventController extends Controller
             ])
         );
         
-        return $event;
+        return new EventResource($event);
     }
 
     /**
