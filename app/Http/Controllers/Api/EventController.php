9c4c92e5eeacd417b3e48904ab16f5740f498c1e
Francislie <mfrancislie@gmail.com>
2024-03-15 05:42:09 -0700

    Protecting routes

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -14,6 +14,10 @@ class EventController extends Controller
 
    private array $relations = ['user', 'attendees', 'attendees.user'];
 
+    public function __contruct(){
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+
     public function index()
     {
         $query = $this->loadRelationships(Event::query());
@@ -38,7 +42,8 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time'
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id
+            // $request->user()->id this an actual user id
         ]);
 
         return new EventResource($this->loadRelationships($event));
